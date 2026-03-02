5a0f9be16ffeb4f8ab8d0e96e41802711f9bef00
mesatyamsharma123 <mesatyamsharma@gmail.com>
2026-03-02 10:07:36 +0530

    Landing View and Home is integrated with google Sign Up Button and Google SignUp Logic

---
+++ GoogleAuthSwiftUI/EnvironmentManager/EnvironmentVariable.swift
@@ -0,0 +1,8 @@
+//
+//  EnvironmentVariable.swift
+//  GoogleAuthSwiftUI
+//
+//  Created by Satyam Sharma Chingari on 01/03/26.
+//
+
+import Foundation
---
+++ GoogleAuthSwiftUI/Views/HomeView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeView.swift
+//  GoogleAuthSwiftUI
+//
+//  Created by Satyam Sharma Chingari on 01/03/26.
+//
+
+import Foundation
